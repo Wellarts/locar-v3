cdd1274306bb66a3c7c49a94a08ece7011a478ca
Wellington Cavalcante <wellarts@gmail.com>
2024-07-31 08:38:42 -0300

    data

--- app/Filament/Resources/ClienteResource.php
+++ app/Filament/Resources/ClienteResource.php
@@ -35,84 +35,87 @@ class ClienteResource extends Resource
                     '2xl' => 3,
                 ])
                     ->schema([
-                Forms\Components\TextInput::make('nome')
-                    ->label('Nome')
-                    ->columnSpan([
-                        'xl' => 2,
-                        '2xl' => 2,
-                    ])
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('cpf_cnpj')
-                    ->label('CPF/CNPJ')
-                    ->mask(RawJs::make(<<<'JS'
+                        Forms\Components\TextInput::make('nome')
+                            ->label('Nome')
+                            ->columnSpan([
+                                'xl' => 2,
+                                '2xl' => 2,
+                            ])
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('cpf_cnpj')
+                            ->label('CPF/CNPJ')
+                            ->mask(RawJs::make(<<<'JS'
                                     $input.length > 14 ? '99.999.999/9999-99' : '999.999.999-99'
                                 JS))
                             ->rule('cpf_ou_cnpj'),
-                Forms\Components\Textarea::make('endereco')
-                    ->label('Endereço')
-                    ->columnSpanFull(),
-                    Forms\Components\Select::make('estado_id')
-                    ->label('Estado')
-                    ->native(false)
-                    ->searchable()
-                    ->required()
-                    ->options(Estado::all()->pluck('nome', 'id')->toArray())
-                    ->reactive(),
-                Forms\Components\Select::make('cidade_id')
-                    ->label('Cidade')
-                    ->native(false)
-                    ->searchable()
-                    ->required()
-                    ->options(function (callable $get) {
-                        $estado = Estado::find($get('estado_id'));
-                        if (!$estado) {
-                            return Estado::all()->pluck('nome', 'id');
-                        }
-                        return $estado->cidade->pluck('nome', 'id');
-                    })
-                    ->reactive(),
-                Forms\Components\TextInput::make('telefone_1')
-                    ->label('Telefone 1')
-                    ->tel()
-                    ->mask('(99)99999-9999'),
-                Forms\Components\TextInput::make('telefone_2')
-                    ->tel()
-                    ->label('Telefone 2')
-                    ->tel()
-                    ->mask('(99)99999-9999'),
-                Forms\Components\TextInput::make('email')
-                    ->columnSpan([
-                        'xl' => 2,
-                        '2xl' => 2,
-                    ])
-                    ->email()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('rede_social')
-                    ->label('Rede Social'),
-                Forms\Components\TextInput::make('cnh')
-                        ->label('CNH'),
-                Forms\Components\TextInput::make('validade_cnh')
-                        ->mask('99/99/9999')
-                        ->maxLength(10)
-                        ->label('Valiade da CNH'),
-                Forms\Components\TextInput::make('rg')
-                        ->label('RG'),
-                Forms\Components\TextInput::make('exp_rg')
-                        ->label('Orgão Exp.'),
-                Forms\Components\Select::make('estado_exp_rg')
-                        ->searchable()
-                        ->label('UF - Expedidor')
-                        ->options(Estado::all()->pluck('nome', 'id')->toArray()),
-                FileUpload::make('img_cnh')
+                        Forms\Components\Textarea::make('endereco')
+                            ->label('Endereço')
+                            ->columnSpanFull(),
+                        Forms\Components\Select::make('estado_id')
+                            ->label('Estado')
+                            ->native(false)
+                            ->searchable()
+                            ->required()
+                            ->options(Estado::all()->pluck('nome', 'id')->toArray())
+                            ->reactive(),
+                        Forms\Components\Select::make('cidade_id')
+                            ->label('Cidade')
+                            ->native(false)
+                            ->searchable()
+                            ->required()
+                            ->options(function (callable $get) {
+                                $estado = Estado::find($get('estado_id'));
+                                if (!$estado) {
+                                    return Estado::all()->pluck('nome', 'id');
+                                }
+                                return $estado->cidade->pluck('nome', 'id');
+                            })
+                            ->reactive(),
+                        Forms\Components\TextInput::make('telefone_1')
+                            ->label('Telefone 1')
+                            ->tel()
+                            ->mask('(99)99999-9999'),
+                        Forms\Components\TextInput::make('telefone_2')
+                            ->tel()
+                            ->label('Telefone 2')
+                            ->tel()
+                            ->mask('(99)99999-9999'),
+                        Forms\Components\TextInput::make('email')
+                            ->columnSpan([
+                                'xl' => 2,
+                                '2xl' => 2,
+                            ])
+                            ->email()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('rede_social')
+                            ->label('Rede Social'),
+                        Forms\Components\TextInput::make('cnh')
+                            ->label('CNH'),
+                        Forms\Components\TextInput::make('validade_cnh')
+                            ->mask('99/99/9999')
+                            ->maxLength(10)
+                            ->label('Valiade da CNH'),
+                        Forms\Components\TextInput::make('rg')
+                            ->label('RG'),
+                        Forms\Components\TextInput::make('exp_rg')
+                            ->label('Orgão Exp.'),
+                        Forms\Components\Select::make('estado_exp_rg')
+                            ->searchable()
+                            ->label('UF - Expedidor')
+                            ->options(Estado::all()->pluck('nome', 'id')->toArray()),
+                        FileUpload::make('img_cnh')
                             ->columnSpan([
                                 'xl' => 2,
                                 '2xl' => 2,
                             ])
                             ->downloadable()
-                        ->label('Foto CNH'),
+                            ->label('Foto CNH'),
 
-                Forms\Components\DatePicker::make('data_nascimento'),
-                ])
+                        Forms\Components\TextInput::make('data_nascimento')
+                            ->mask('99/99/9999')
+                            ->maxLength(10)
+                            ->label('Data de Nascimento'),
+                    ])
             ]);
     }
 
@@ -120,28 +123,28 @@ class ClienteResource extends Resource
     {
         return $table
             ->columns([
-            Tables\Columns\TextColumn::make('nome')
-                ->sortable()
-                ->searchable(),
-            Tables\Columns\TextColumn::make('endereco')
-                ->label('Endereço'),
-            Tables\Columns\TextColumn::make('estado.nome')
-                ->label('Estado'),
-            Tables\Columns\TextColumn::make('cidade.nome')
-                ->label('Cidade'),
-            Tables\Columns\TextColumn::make('telefone_1')
+                Tables\Columns\TextColumn::make('nome')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('endereco')
+                    ->label('Endereço'),
+                Tables\Columns\TextColumn::make('estado.nome')
+                    ->label('Estado'),
+                Tables\Columns\TextColumn::make('cidade.nome')
+                    ->label('Cidade'),
+                Tables\Columns\TextColumn::make('telefone_1')
 
-                ->formatStateUsing(fn (string $state) => vsprintf('(%d%d)%d%d%d%d%d-%d%d%d%d', str_split($state)))
-                ->label('Telefone'),
+                    ->formatStateUsing(fn (string $state) => vsprintf('(%d%d)%d%d%d%d%d-%d%d%d%d', str_split($state)))
+                    ->label('Telefone'),
 
-            Tables\Columns\TextColumn::make('email')
-                ->label('Email'),
-            Tables\Columns\TextColumn::make('created_at')
-                ->toggleable(isToggledHiddenByDefault: true)
-                ->dateTime(),
-            Tables\Columns\TextColumn::make('updated_at')
-                ->dateTime()
-                ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Email'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->toggleable(isToggledHiddenByDefault: true)
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->toggleable(isToggledHiddenByDefault: true),
             ])
             ->filters([
                 //
