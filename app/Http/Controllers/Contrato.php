c13b7c7e6c25e555d79887a8b36036d10548969c
Wellington Cavalcante <wellarts@gmail.com>
2024-11-30 12:06:28 -0300

    contrato

--- app/Http/Controllers/Contrato.php
+++ app/Http/Controllers/Contrato.php
@@ -33,8 +33,8 @@ class Contrato extends Controller
         //FORMATAR TELEFONE
          $tel_1 = $locacao->Cliente->telefone_1;
          $tel_2 = $locacao->Cliente->telefone_2;
-       //  $tel_1 = " (".substr($tel_1, 0, 2).") ".substr($tel_1, 2, 5)."-".substr($tel_1, 7, 11);
-       //  $tel_2 = " (".substr($tel_2, 0, 2).") ".substr($tel_2, 2, 5)."-".substr($tel_2, 7, 11);
+         $locacao->valor_desconto = number_format($locacao->valor_desconto,  2, ",", ".");
+         $locacao->valor_total_desconto = number_format($locacao->valor_total_desconto, 2, ",", ".");
 
 
 
@@ -45,13 +45,7 @@ class Contrato extends Controller
                                                         'tel_1',
                                                         'tel_2']))
                                                         ->stream();
-                                                        
-        // return view('pdf.locacao.contrato', compact(['locacao',
-        //                                     'dataAtual',
-        //                                     'cpfCnpj',
-        //                                     'tel_1',
-        //                                     'tel_2']));
 
-
+        
     }
 }
